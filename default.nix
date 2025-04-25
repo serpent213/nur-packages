9fcb6abe7fd4431f796a5b2c74af6840da4e494b
Steffen Beyer <steffen@beyer.io>
2025-04-25 09:50:33 +0200

    Introduce pushlog module

--- default.nix
+++ default.nix
@@ -8,10 +8,10 @@
 {pkgs ? import <nixpkgs> {}}: {
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib {inherit pkgs;}; # functions
-  modules = import ./modules; # NixOS modules
+  modules = import ./modules {inherit pkgs;}; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package {};
+  # example-package = pkgs.callPackage ./pkgs/example-package {};
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
